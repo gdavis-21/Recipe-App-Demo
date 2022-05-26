e24ca23dee3234ed873489d623fc464dc5408243
gdavis-21 <gdavis19@uab.edu>
2022-05-25 20:42:15 -0500

    Add RecipeTabView as default start-up screen.

---
+++ Recipe_List_App/View/RecipeTabView.swift
@@ -0,0 +1,38 @@
+//
+//  RecipeTabView.swift
+//  Recipe_List_App
+//
+//  Created by Grant Davis on 5/25/22.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        
+        TabView {
+            // MARK: First Tab: Featured
+            Text("Featured View")
+                .tabItem({
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                })
+            // MARK: Second Tab: All recipes
+            RecipeListView()
+                .tabItem({
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                })
+        }
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
