927da7a2eb3e1164508e534387d231a8095a1f2a
gdavis-21 <gdavis19@uab.edu>
2022-05-31 18:08:56 -0500

    - Add RecipeFeaturedView - Add environmentObject modifier - Modify dataflow with environmentObject - Create instance of RecipeFeaturedView in RecipeTabView

---
+++ Recipe_List_App/View/RecipeFeaturedView.swift
@@ -0,0 +1,23 @@
+//
+//  RecipeFeaturedView.swift
+//  Recipe_List_App
+//
+//  Created by Grant Davis on 5/31/22.
+//
+
+import SwiftUI
+
+struct RecipeFeaturedView: View {
+    
+    @EnvironmentObject var model: RecipeModel
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeFeaturedView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeFeaturedView()
+    }
+}
--- Recipe_List_App/View/RecipeListView.swift
+++ Recipe_List_App/View/RecipeListView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct RecipeListView: View {
     
     // Reference ViewModel
-    @ObservedObject var model = RecipeModel()
+    @EnvironmentObject var model: RecipeModel
     
     var body: some View {
         NavigationView {
