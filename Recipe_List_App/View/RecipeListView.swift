acb7212179f04e47dc62e076a20fffd7580bd10a
gdavis-21 <gdavis19@uab.edu>
2022-06-02 18:34:01 -0500

    - Change Title from a navigation view title to text in VStack - Change List in RecipeList to ScrollView, ForEach, and LazyVStack

--- Recipe_List_App/View/RecipeListView.swift
+++ Recipe_List_App/View/RecipeListView.swift
@@ -14,22 +14,38 @@ struct RecipeListView: View {
     
     var body: some View {
         NavigationView {
-            List(model.recipes) { r in
-                NavigationLink(destination: {
-                    RecipeDetailView(recipe: r)
-                }, label: {
-                    // MARK: Row Item
-                    HStack {
-                        Image(r.image)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 50, height: 50, alignment: .center)
-                            .clipped()
-                            .cornerRadius(5)
-                        Text(r.name)
+            
+            VStack(alignment: .leading) {
+                Text("All Recipes")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding(.top, 40)
+                ScrollView {
+                    VStack {
+                        LazyVStack(alignment: .leading) {
+                            ForEach(model.recipes) { r in
+                                NavigationLink(destination: {
+                                    RecipeDetailView(recipe: r)
+                                }, label: {
+                                    // MARK: Row Item
+                                    HStack {
+                                        Image(r.image)
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 50, height: 50, alignment: .center)
+                                            .clipped()
+                                            .cornerRadius(5)
+                                        Text(r.name)
+                                            .foregroundColor(.black)
+                                    }
+                                })
+                            }
+                        }
                     }
-                })
-            }.navigationTitle(Text("All Recipes"))
+                }
+            }
+            .navigationBarHidden(true)
+            .padding(.leading)
         }
     }
 }
@@ -37,5 +53,6 @@ struct RecipeListView: View {
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
+            .environmentObject(RecipeModel())
     }
 }
